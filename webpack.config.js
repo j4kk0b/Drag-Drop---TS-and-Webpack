59987c7879df5b59eb2675721cee1dc98891128a
Jakub Kozanecki <jakubkozaneckidev@gmail.com>
2022-10-26 19:57:46 +0200

    First push

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    // clear: true
+    publicPath: "/dist/",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./"),
+    },
+  },
+  resolve: {
+    extensions: [
+      // it is an array
+      ".ts",
+      ".js",
+    ],
+  },
+};
